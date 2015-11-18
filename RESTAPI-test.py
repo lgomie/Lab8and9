a5de931e905d8e9ea61150d8a3906cdcc136a9f4
Luiz Gomes <luiz.fs.gomes@gmail.com>
2015-11-18 15:48:47 +0000

    message

--- RESTAPI-test.py
+++ RESTAPI-test.py
@@ -2,7 +2,7 @@
 
 commands = {'List all containers', 'List running containers', 'Inspect a container', 'Delete a container', 'Delete all containers', 'Create a container from an image', 'Restart a container', 'Stop a container' ,'Show logs from a container', 'List all images', 'Delete an image', 'Delete all images', 'TAG an image', 'create Docker images from local Dockerfile'};
 
-no = 1
+no = 0
 for i in commands:
-    print no
+    print no, "-", i
     no = no + 1
